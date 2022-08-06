fcd704c23d7b43fdbc87e0fb118df3a1385ce5ee
Romain DESSART <romaindessart@gmail.com>
2022-08-06 18:47:27 +0200

    Project Creation

---
+++ eSkystudio.A320.Simulator.PFD/Program.cs
@@ -0,0 +1,21 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+
+RenderWindow win = new(new (480, 640),"A320-PFD");
+win.SetFramerateLimit(20);
+win.KeyPressed += (sender, ev) =>
+{
+    if (ev.Code == Keyboard.Key.Escape)
+    {
+        win.Close();
+    }
+};
+
+while (win.IsOpen)
+{
+    win.DispatchEvents();
+    win.Clear();
+    win.Display();
+}
+
+win.Close();
